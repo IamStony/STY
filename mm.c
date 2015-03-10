ae6c696d35f86a630022ce469addccfc448d23fb
steinara13 <steinara13@skel.ru.is>
2015-03-10 17:27:50 +0000

    Adding comments and minor changes, Revert to the last commit if something breaks

--- mm.c
+++ mm.c
@@ -1,15 +1,40 @@
 /*
- * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
- * In this naive approach, a block is allocated by simply incrementing
- * the brk pointer.  A block is pure payload. There are no headers or
- * footers.  Blocks are never coalesced or reused. Realloc is
- * implemented directly using mm_malloc and mm_free.
+ * doublylinked free list
  *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
+ *  We're using 'nodes' to keep a record of our memory and we always point directly to the payload
+ * of each node.
+ *          
+ * <------Header------>                 <------Footer----->
+ *  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+ * |         |         |               |         |         |
+ * |  Size   | Pointer |               | Pointer |  Size   |
+ * |         |   to    |               |   to    |         |
+ * |         |  next   |    Payload    |  next   |         |
+ * |_ _ _ _ _|  free   |               |  free   |_ _ _ _ _|
+ * |Allocated|  node   |               |  node   |Allocated|
+ * |_ _ _ _ _|_ _ _ _ _|_ _ _ _ _ _ _ _|_ _ _ _ _|_ _ _ _ _|
+ *   4bytes    4bytes                    4bytes    4bytes
  *
- */
+ *  There's always a 'head' node iniated in mm_init. It's solely there to point 
+ * to the start of the free list and make sure we don't coalesce beyond 
+ * mem_heap_lo(). It has no payload so it takes up exactly 16bytes. 
+ *  The first 4 bytes of a node contain both the size of the payload and
+ * the allocated bit. The last 4 words also contain these information.
+ *  If malloc doesn't find a node in the free list to use then we sbrk for more memory
+ * and add an extra page to it so we don't have to call it as often. This is of course added
+ * to the free list.
+ *  Free nodes are split up if there's an excess of 32 or more bytes, 
+ * 16 reserved for the node header/footer and the rest is always the payload.
+ * Keeping the newly split node in the free list (adds it to the front) and allocating
+ * the space we need.
+ *  Newly freed blocks are inserted at the front of the list, behind the 'head'
+ *  -e.g. 
+ *            ->|             ->         ->         -> NULL
+ *         head |       node(c)    node(b)    node(a) 
+ *   NULL <-    | NULL <-         <-         <-
+ *
+ *
+ * */
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -76,12 +101,13 @@ team_t team = {
 #define LASTN(bp)           ((void *)(bp) - (GET_SIZE(LNF(bp)) + NODESIZE))
 #define NEXTN(bp)           ((void *)(bp) + (GET_SIZE(NH(bp)) + NODESIZE))
 
+//All print functions we're used for debuggin purposes
 void print_list();
 void print_heap();
 void print_node(void *node);
 void print_pcn(void *node);
-void *coalesce(void *bp);
-void *find_fit(size_t size);
+void coalesce(void *bp);
+void *find_first(size_t size);
 void split(void *bp, size_t size);
 void add_node(void *bp);
 void rm_node(void *bp);
@@ -103,18 +129,19 @@ int mm_init(void)
      * The prev pointer always points to NULL, should never be used
      * Footer is marked as allocated with size = 0
      */
-    head = mem_sbrk(NODESIZE);
-    head += DSIZE;
-    PUT(NH(head), PACK(0, 1));
-    PUT(NP(head), 0);
-    PUT(PP(head), 0);
-    PUT(NF(head), PACK(0, 1));
-
-    //print_node(head);
+    head = mem_sbrk(NODESIZE);  //Extends the heap to initialize our head
+    head += DSIZE;              //Puts us at the payload
+    PUT(NH(head), PACK(0, 1));  //Puts the size of the header as 0 and marks it as allocated
+    PUT(NP(head), 0);           //head->next = NULL
+    PUT(PP(head), 0);           //head->prev = NULL
+    PUT(NF(head), PACK(0, 1));  //Puts the size of the footer as 0 and marks it as allocated
 
     return 0;
 }
 
+/*
+ * print_list - prints the whole free list, head included
+ * */
 void print_list()
 {
     void *bp;
@@ -124,44 +151,49 @@ void print_list()
     }
 }
 
+/*
+ * print_heap - prints the whole heap, head included
+ * */
 void print_heap()
 {
     void *bp;
-    int i = -1;
     for(bp = head; bp < (void *)mem_heap_hi(); bp = NEXTN(bp))
     {
-        printf("Node: %d\n", i);
         print_node(bp);
-        i = i + 1;
     }
+    //Just to check if the last node is occupying the same end of the heap
     printf("Mem heap hi: %p\n\n", (void *)mem_heap_hi());
 }
 
+/*
+ * print_node - prints the node information
+ * */
 void print_node(void *node) 
 {
     //printf("Printing node block:\n");
-    printf("%s block.\n", IS_ALLOC(NH(node))?"Allocated":"Free");
-    printf("Header: %p\n", NH(node));
-    printf("H_size = %d\n", GET_SIZE(NH(node)));
-    //printf("Next node: %p\n", NP(node));
-    printf("Next node address: %p\n", NPA(node));
-    printf("Payload: %p\n", node);
-    //printf("Prev node: %p\n", PP(node));
-    printf("Prev node address: %p\n", PPA(node));
-    printf("Footer: %p\n", NF(node));
-    printf("F_size = %d\n\n", GET_SIZE(NF(node)));
-    fflush(stdout);
+    printf("%s block.\n", IS_ALLOC(NH(node))?"Allocated":"Free");   //Is the block allocated or free?
+    printf("Header: %p\n", NH(node));                               //Header address
+    printf("H_size = %d\n", GET_SIZE(NH(node)));                    //Size in the header
+    //printf("Next node: %p\n", NP(node));                          //Next node address (just to check nodes are set up correctly)
+    printf("Next node address: %p\n", NPA(node));                   //Gets the address stored at Next node
+    printf("Payload: %p\n", node);                                  //Payload start address
+    //printf("Prev node: %p\n", PP(node));                          //Previous node address (just to check nodes are set up correctly)
+    printf("Prev node address: %p\n", PPA(node));                   //Gets the address stored at Previous node
+    printf("Footer: %p\n", NF(node));                               //Footer address
+    printf("F_size = %d\n\n", GET_SIZE(NF(node)));                  //Size in the footer
 }
 
+/*
+ * print_pcn - Prints previous, current and next node based on current from heap
+ * */
 void print_pcn(void *node)
 {
     printf("- - - - - - - - - - - - - - - - - - -\n");
-    print_node(LASTN(node));
+    print_node(LASTN(node));                                        //Prints the previous node on the heap
     printf("                - -                  \n");
-    print_node(node);
+    print_node(node);                                               //Prints the current node we're on in the heap
     printf("                - -                  \n");
-    print_node
-    (NEXTN(node));
+    print_node(NEXTN(node));                                        //Prinst the next node on the heap
     printf("- - - - - - - - - - - - - - - - - - -\n");
 }
 
@@ -180,22 +212,24 @@ void *mm_malloc(size_t size)
         return NULL;
     }
 
-    asize = ALIGN(size);
+    asize = ALIGN(size);    //Aligs size of the requested payload to 8
 
-    bp = find_fit(asize);
+    bp = find_first(asize);   //Searches the free list for a node with enough payload
 
-    if(bp != NULL)
+    if(bp != NULL) //If a node is found
     {
         size_t leftover;
-        leftover = GET_SIZE(NH(bp)) - asize;          //Size of leftover space
+        leftover = GET_SIZE(NH(bp)) - asize;    //Size of leftover/unused space in the node's payload
+        
         //If leftover space is enough to create a new node 
-        //AND has a size >= 16 then we make a new free block
+        //AND has a payload size >= 16 then we make a new free block
         if((int)leftover >= (int)MIN_SIZE)
         {
             split(bp, asize);
         }
         else
         {
+            //Remove the node from the free list and mark it as allocated
             rm_node(bp);
             PUT(NH(bp), PACK(GET_SIZE(NH(bp)), 1));
             PUT(NF(bp), PACK(GET_SIZE(NH(bp)), 1));
@@ -205,49 +239,45 @@ void *mm_malloc(size_t size)
     {
         size_t nnsize; //New Node Size
         nnsize = asize + NODESIZE;
-        if(nnsize % 8 != 0)
-        {
-            printf("Not aligning correctly you numnuts!(malloc loves you)");
-            fflush(stdout);
-            return NULL;
-        }
+
         bp = mem_sbrk(nnsize);
+        //if sbrk was unsuccessful
         if((long)bp == -1)
         {
             return NULL;
         }
         else
         {
-            bp += DSIZE;    //Puts us at payload
-            PUT(NH(bp), PACK(asize, 1));
-            PUT(NP(bp), 0);
-            PUT(PP(bp), 0);
-            PUT(NF(bp), PACK(asize, 1));
+            bp += DSIZE;                  //Puts us at payload (past the header)
+            PUT(NH(bp), PACK(asize, 1));  //Sets the header
+            PUT(NP(bp), 0);               //bp->next = NULL
+            PUT(PP(bp), 0);               //bp->prev = NULL
+            PUT(NF(bp), PACK(asize, 1));  //Sets the footer
         }
     }
     
-    return bp;
-}
+    return bp;  //Returns the pointer to the payload for allocation
+}   
 
 /*
- * mm_free - Freeing a block does nothing.
+ * mm_free - Calls coalesce and free's the node.
  */
 void mm_free(void *bp)
 {
+    //Check if called correctly
     if(bp != NULL)
     {
+        //Mark the node as free before coalescing
         size_t size = GET_SIZE(NH(bp));
-
         PUT(NH(bp), PACK(size, 0));
         PUT(NF(bp), PACK(size, 0));
+        
         coalesce(bp);
-        //add_node(bp);
     }
 }
 
-void *coalesce(void *bp)
+void coalesce(void *bp)
 {
-    //printf("Called coalesce.\n");
     void *p, *n;
     p = LASTN(bp);  //Previous node
     n = NEXTN(bp);  //Next node
@@ -255,29 +285,30 @@ void *coalesce(void *bp)
     size_t prev_alloc = IS_ALLOC(NH(p));    //Previous node alloc bit
     size_t next_alloc = IS_ALLOC(NH(n));    //Next node alloc bit
     size_t size = GET_SIZE(NH(bp));         //Size of current node payload
+    //If the next node is outside the heap
     if(n > (void *)mem_heap_hi())
     {
-        //printf("n is outside mem_heap_hi()\n");
-        next_alloc = 1;
+        next_alloc = 1; //Mark the non existance node as allocated to skip coalescing to the right
     }
-    if(!next_alloc)     //If next node is free
+    //If node to the right of bp on the heap is free
+    if(!next_alloc)
     {
         size += GET_SIZE(NH(n)) + NODESIZE;
         rm_node(n);                 //Removes next node from free list
         PUT(NH(bp), PACK(size, 0));
         PUT(NF(bp), PACK(size, 0));
     }
+    //If node to the left of bp on the heap is free
     if(!prev_alloc)
     {
         size += GET_SIZE(NH(p)) + NODESIZE;
         rm_node(p);                 //Removes previous node from free list
         bp = p;                     //Moves us the the previous nodes payload address
-        PUT(NH(bp), PACK(size, 0));
-        PUT(NF(bp), PACK(size, 0));
+        PUT(NH(bp), PACK(size, 0)); //Update header size and marks us as a free node
+        PUT(NF(bp), PACK(size, 0)); //Update footer size and marks us as a free node
     }
 
     add_node(bp);   //Adds coalesced node to free list
-    return NULL;
 }
 
 /*
@@ -302,14 +333,15 @@ void *mm_realloc(void *ptr, size_t size)
 }
 
 /*
- * size is the payload
+ * find_fit - Finds the first free node that can hold the asked for payload (size)
  * */
-void *find_fit(size_t size)
+void *find_first(size_t size)
 {
     void *bp;
-
+    //Starts search at the first free node head points to
     for(bp = NPA(head); bp != NULL; bp = NPA(bp))
     {
+        //As soon as we find a node that has a big enough payload we return it
         if(size <= GET_SIZE(NH(bp)))
         {
             return bp;
@@ -334,15 +366,14 @@ void split(void *bp, size_t size)
     PUT(PP(bp), 0);
     PUT(NF(bp), PACK(size, 1));
 
-    //Create the new 'empty' node
+    //Create the new 'empty' node with the leftover size (-16 bytes for creating the new node)
     node = bp + (size + NODESIZE);  //Puts us at the payload of the new node
     PUT(NH(node), PACK(losize, 0));
     PUT(NP(node), 0);
     PUT(PP(node), 0);
     PUT(NF(node), PACK(losize, 0));
 
-    coalesce(node);
-    //add_node(node); //Add the new free node to the list
+    coalesce(node); //Coalesce the new free block
 }
 
 void add_node(void *bp)
